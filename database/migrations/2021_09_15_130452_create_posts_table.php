868c7db256b2189700552f3891e454a758cf948e
vvalcarcell <valcarcellvalentina@gmail.com>
2021-09-15 19:47:47 +0200

    autenticazione

---
+++ database/migrations/2021_09_15_130452_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->char('title', 150);
+            $table->char('author', 50);
+            $table->text('img_path');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0;$i<50; $i++){
+            $post= new Post();
+            $post->title= $faker->sentence(5);
+            $post->author= $faker->words(2,true);
+            $post->img_path= $faker->imageUrl(360, 360);
+            $post->date= $faker->date();
+            $post->save();
+
+        }
+    }
+}
