51fdc491ba37e2882df91cec4191268854a0f5c0
vvalcarcell <valcarcellvalentina@gmail.com>
2021-09-22 13:09:17 +0200

    new table created

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable= ['author', 'title', 'img_path'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
 }
