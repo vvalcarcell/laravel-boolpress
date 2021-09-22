74b41a005768b47f6f3089b522fe4cdf79eb1cde
vvalcarcell <valcarcellvalentina@gmail.com>
2021-09-22 15:01:58 +0200

    new sedeer

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Post;
+use App\Category;
 use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
@@ -13,11 +14,45 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $categoryList = [
+            'cronaca',
+            'sport',
+            'opinione',
+            'musica',
+            'cinema',
+            'moda'
+        ];
+
+        
+        $listOfCategoryID = [];  
+
+        foreach($categoryList as $category) {
+
+            $categoryObject = new Category();
+
+            $categoryObject->name = $category;
+            $categoryObject->save();
+            
+            $listOfCategoryID[] = $categoryObject->id;
+        }
+
+
+
+
+
         for($i=0;$i<50; $i++){
             $post= new Post();
             $post->title= $faker->sentence(5);
             $post->author= $faker->words(2,true);
-            $post->img_path= $faker->imageUrl(60, 60);
+            $post->img_path= $faker->imageUrl(360, 360);
+
+            //dalla lista degli indici ne estrae uno random restituendoci il suo indice nella lista
+            $randIndexofIndex = array_rand($listOfCategoryID, 1);
+            //utilizzando l'indice randomico salviamo il valore corrispondente nella variabile $categoryID
+            $categoryID = $listOfCategoryID[$randIndexofIndex];
+            //assegno questo valore alla proprietà category_id
+            $post->category_id = $categoryID;
+
             $post->save();
 
         }
